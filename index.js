62b8cdf52c8dd8a25a2f6a652eaf665b509a3457
Bruno Henrique <b.henrique261991@gmail.com>
2021-12-13 19:24:34 -0200

    Criando metodo Transferir

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-class Cliente{
-    nome;
-    cpf;
-}
-
-class ContaCorrente{
-    agencia;
-    saldo;
-  
-   sacar(valor) {
-       if(this.saldo >= valor){
-           this.saldo -= valor;
-       }
-   }
-
-    depositar(valor) {
-        if(valor >= 0){
-            this.saldo += valor;
-        }
-    }
-
-}
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
@@ -30,11 +10,17 @@ cliente2.nome = "Alice";
 cliente2.cpf = 88112233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.sacar(50);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
 
-console.log(contaCorrenteRicardo);
+contaCorrenteRicardo.transferir(200, conta2);
+
+console.log(conta2);
+
+
 
