8f12082fb6928e285a9899c388890ff6624d1a70
Bruno Henrique <b.henrique261991@gmail.com>
2021-12-14 17:53:20 -0200

    Criando Henrancas entre Classes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Conta } from "./conta.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const cliente2 = new Cliente("Alice", 88112233309);
-
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
-const conta2 = new ContaCorrente(102, cliente2);
+contaCorrenteRicardo.sacar(100);
 
-let valor = 200;
-contaCorrenteRicardo.transferir(valor, conta2);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
-console.log(cliente2.cpf);
+console.log(contaPoupanca);
+console.log(contaCorrenteRicardo);
 
 
 
