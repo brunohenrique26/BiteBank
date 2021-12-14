eac0d26f1500b49cc7ea28189cadb8af15c67111
Bruno Henrique <b.henrique261991@gmail.com>
2021-12-14 21:29:42 -0200

    criando Herancas e classes abstratas

--- Conta.js
+++ Conta.js
@@ -1,39 +1,49 @@
-export class Conta{
-    constructor(saldoInicial, cliente, agencia){
+// Classe abstrata, não pode ser instanciada só pode ser herdada
+export class Conta {
+    constructor(saldoInicial, cliente, agencia) {
+        if(this.constructor == Conta){
+            throw new Error("Voçê não deveria instanciar um objeto do tipo Conta diretamente, pois essa é uma classe abstrata");
+        }
+
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
     }
 
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente){
-        this._cliente = novoValor;
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
         }
     }
 
-    get cliente(){
+    get cliente() {
         return this._cliente;
     }
 
-    get saldo (){
-       return this._saldo;
+    get saldo() {
+        return this._saldo;
     }
 
     sacar(valor) {
-        let taxa = 1;
+        throw new Error("O metodo Sacar da conta é abstrato")
+    }
+
+    _sacar(valor, taxa){
         const valorSacado = taxa * valor;
-        if(this._saldo >= valorSacado){
+        if (this._saldo >= valorSacado) {
             this._saldo -= valorSacado;
             return valorSacado;
         }
+
+        return 0;
     }
- 
-     depositar(valor) {
-         this._saldo += valor;
-     }
- 
-     transferir(valor, conta){
-         const valorSacado = this.sacar(valor);
-         conta.depositar(valorSacado);
-     }
+
+    depositar(valor) {
+        this._saldo += valor;
+    }
+
+    tranferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
 }
